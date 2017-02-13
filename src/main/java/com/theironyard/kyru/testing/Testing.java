f0ed8924450fd2e8df0b5ece63bb2127904c2cc9
Kyle Rudy <kdrudy@kyru.net>
2017-02-13 07:38:22 -0600

    Updated to start working with twitter

--- src/main/java/com/theironyard/kyru/testing/Testing.java
+++ src/main/java/com/theironyard/kyru/testing/Testing.java
@@ -1,24 +1,77 @@
 package com.theironyard.kyru.testing;
 
+import twitter4j.*;
+
 import java.io.*;
+import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by kdrudy on 7/25/16.
  */
 public class Testing {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws TwitterException {
         System.out.println("Testing");
 
         Testing t = new Testing();
 
-        t.mersennePrime();
+//        t.mersennePrime();
 
-//        for(int i = 0;i<10;i++) {
-//            System.out.println(t.dissociatedPressFormatted(3, 1, 15, new File("./resources/timecube.txt")));
-//        }
+        Twitter twitter = TwitterFactory.getSingleton();
+        System.out.println(twitter.getRateLimitStatus().get("/statuses/user_timeline"));
+        Paging p = new Paging();
+        p.setCount(200);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 10; i++) {
+            p.setPage(i);
+            List<Status> statuses = twitter.getHomeTimeline(p);
+
+            for (Status s : statuses) {
+                if (!s.getText().startsWith("RT")) {
+                    sb.append(". " + s.getText());
+                }
+
+            }
+        }
+
+
+        //String dp = t.dissociatedPress(3, 1, 100, "./resources/timecube.txt");
+        String dp = t.dissociatedPress(3, 1, 100, sb);
+
+        System.out.println(dp);
+
+        ArrayList<String> sentences = t.extractSentences(dp);
+
+        for(String s : sentences) {
+            System.out.println();
+            System.out.println(s);
+        }
+
+    }
+
+    private ArrayList<String> extractSentences(String dp) {
+        ArrayList<String> sentences = new ArrayList<>();
+        Pattern pattern = Pattern.compile("([?.]\\s+[A-Z*@])");
+        Matcher m = pattern.matcher(dp);
+
+        m.find();
+        int startIndex = m.start() + 1;
+        int endIndex = dp.length();
+        while (m.find()) {
+            endIndex = m.end() - 1;
+
+            sentences.add(dp.substring(startIndex, endIndex));
+
+            startIndex = m.start() + 1;
+        }
+
+        return sentences;
     }
 
     private void mersennePrime() {
@@ -33,12 +86,12 @@ public class Testing {
 //        31:2147483647:16ms
 //        61:2305843009213693951:111887ms
 
-        for(int i = 0;i<100;i++) {
+        for (int i = 0; i < 100; i++) {
             BigInteger value = BigInteger.valueOf(2).pow(i).subtract(BigInteger.ONE);
             long time = System.currentTimeMillis();
 
-            if(isPrime(value)) {
-                System.out.println(i + ":" + value + ":" + (System.currentTimeMillis()-time) + "ms");
+            if (isPrime(value)) {
+                System.out.println(i + ":" + value + ":" + (System.currentTimeMillis() - time) + "ms");
             } else {
                 //System.out.println("NO " + i + ":" + value);
             }
@@ -48,14 +101,23 @@ public class Testing {
     public boolean isPrime(int value) {
         return isPrime(new Long(value));
     }
-    public boolean isPrime(long value) {
-        if(value == 0 || value == 1) { return false; }
-        if(value == 2) { return true; }
-        if(value%2 == 0) { return false; }
 
-        int max = (int)Math.sqrt(value) + 1;
-        for(int i = 3;i < max; i+=2) {
-            if(value % i == 0) { return false; }
+    public boolean isPrime(long value) {
+        if (value == 0 || value == 1) {
+            return false;
+        }
+        if (value == 2) {
+            return true;
+        }
+        if (value % 2 == 0) {
+            return false;
+        }
+
+        int max = (int) Math.sqrt(value) + 1;
+        for (int i = 3; i < max; i += 2) {
+            if (value % i == 0) {
+                return false;
+            }
         }
         return true;
     }
@@ -63,13 +125,19 @@ public class Testing {
     public boolean isPrime(BigInteger value) {
         BigInteger two = new BigInteger("2");
 
-        if(value.equals(BigInteger.ZERO) || value.equals(BigInteger.ONE)) { return false; }
-        if(value.equals(two)) { return true; }
-        if(value.mod(two).equals(BigInteger.ZERO)) { return false; }
+        if (value.equals(BigInteger.ZERO) || value.equals(BigInteger.ONE)) {
+            return false;
+        }
+        if (value.equals(two)) {
+            return true;
+        }
+        if (value.mod(two).equals(BigInteger.ZERO)) {
+            return false;
+        }
 
         BigInteger max = value.divide(BigInteger.valueOf(2));
-        for(BigInteger bi = BigInteger.valueOf(3); bi.compareTo(max) < 0; bi = bi.add(two)) {
-            if(value.mod(bi).equals(BigInteger.ZERO)) {
+        for (BigInteger bi = BigInteger.valueOf(3); bi.compareTo(max) < 0; bi = bi.add(two)) {
+            if (value.mod(bi).equals(BigInteger.ZERO)) {
                 return false;
             }
             max = value.divide(bi);
@@ -92,90 +160,95 @@ public class Testing {
                 sentence = sentence.trim() + ".";
             }
 
-        } while(sentence.split(" ").length < 5); //Ensure we have some kind of sentence and not just a few words
+        } while (sentence.split(" ").length < 5); //Ensure we have some kind of sentence and not just a few words
 
         return sentence;
     }
 
+
+    public String dissociatedPress(int pullWords, int match, int total, StringBuilder sb) {
+        Scanner scanner = new Scanner(sb.toString());
+        return dissociatedPress(pullWords, match, total, scanner);
+    }
+
     public String dissociatedPress(int pullWords, int match, int total, String filename) {
         try {
+            Scanner scanner = new Scanner(new File(filename));
 
-            //Read in the file and split up the words
-            //FileReader fr = new FileReader(textFile);
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    this.getClass().getClassLoader().getResourceAsStream(filename)));
-
-            ArrayList<String> textWords = new ArrayList<>();
-
-            while(br.ready()) {
-                String line = br.readLine();
-                String[] words = line.split(" ");
-                for(String word : words) {
-                    if(!"".equals(word.trim())) {
-                        textWords.add(word);
-                    }
-                }
-            }
-
-            //Find a random starting point
-            int totalWords = textWords.size();
-            int startingPoint = (int)(Math.random()*(totalWords - pullWords));
-
-            //Pull in initial words and set up match words
-            String[] nextMatch = new String[match];
-            StringBuilder newText = new StringBuilder();
-            int wordCount = 0;
-
-            for(int i = 0;i<pullWords;i++) {
-                newText.append(textWords.get(startingPoint+i));
-                newText.append(" ");
-                wordCount++;
-                if(i - (pullWords - match) >= 0) {
-                    nextMatch[i - (pullWords - match)] = textWords.get(startingPoint+i);
-                }
-            }
-
-            //Continue matching until we get all the words we want
-            while(wordCount < total) {
-                ArrayList<Integer> possibleNewStart = new ArrayList<>();
-                //Find next match
-                for(int i = 0;i<totalWords;i++) {
-                    if (textWords.get(i).equals(nextMatch[0])) {
-                        boolean validMatch = true;
-                        for (int j = 0; j < match; j++) {
-                            if (!textWords.get(i + j).equals(nextMatch[j])) {
-                                validMatch = false;
-                            }
-                        }
-                        if (validMatch) {
-                            possibleNewStart.add(i);
-                        }
-                    }
-                }
-                int newStart = (int)(Math.random()*possibleNewStart.size());
-
-                for(int i = 0;i<pullWords;i++) {
-                    newText.append(textWords.get(possibleNewStart.get(newStart)+nextMatch.length+i));
-                    newText.append(" ");
-                    wordCount++;
-                    if(i - (pullWords - match) >= 0) {
-                        nextMatch[i - (pullWords - match)] = textWords.get(possibleNewStart.get(newStart)+nextMatch.length+i);
-                    }
-                }
-            }
-
-            //Print out line
-            return newText.toString();
-
+            return dissociatedPress(pullWords, match, total, scanner);
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found: " + filename);
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String dissociatedPress(int pullWords, int match, int total, Scanner scanner) {
+
+        //Read in the file and split up the words
+        //FileReader fr = new FileReader(textFile);
+
+        ArrayList<String> textWords = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] words = line.split(" ");
+            for (String word : words) {
+                if (!"".equals(word.trim())) {
+                    textWords.add(word);
+                }
+            }
         }
 
-        return new String();
+        //Find a random starting point
+        int totalWords = textWords.size();
+        int startingPoint = (int) (Math.random() * (totalWords - pullWords));
+
+        //Pull in initial words and set up match words
+        String[] nextMatch = new String[match];
+        StringBuilder newText = new StringBuilder();
+        int wordCount = 0;
+
+        for (int i = 0; i < pullWords; i++) {
+            newText.append(textWords.get(startingPoint + i));
+            newText.append(" ");
+            wordCount++;
+            if (i - (pullWords - match) >= 0) {
+                nextMatch[i - (pullWords - match)] = textWords.get(startingPoint + i);
+            }
+        }
+
+        //Continue matching until we get all the words we want
+        while (wordCount < total) {
+            ArrayList<Integer> possibleNewStart = new ArrayList<>();
+            //Find next match
+            for (int i = 0; i < totalWords; i++) {
+                if (textWords.get(i).equals(nextMatch[0])) {
+                    boolean validMatch = true;
+                    for (int j = 0; j < match; j++) {
+                        if (!textWords.get(i + j).equals(nextMatch[j])) {
+                            validMatch = false;
+                        }
+                    }
+                    if (validMatch) {
+                        possibleNewStart.add(i);
+                    }
+                }
+            }
+            int newStart = (int) (Math.random() * possibleNewStart.size());
+
+            for (int i = 0; i < pullWords; i++) {
+                newText.append(textWords.get(possibleNewStart.get(newStart) + nextMatch.length + i));
+                newText.append(" ");
+                wordCount++;
+                if (i - (pullWords - match) >= 0) {
+                    nextMatch[i - (pullWords - match)] = textWords.get(possibleNewStart.get(newStart) + nextMatch.length + i);
+                }
+            }
+        }
+
+        //Print out line
+        return newText.toString();
     }
 
     public boolean isInteger(String str) {
